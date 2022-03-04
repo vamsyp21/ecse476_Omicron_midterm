075773e36f6d00c6a8ad7bd87075d91cfdbfceba
Vamsy <vamsyp21@gmail.com>
2022-03-04 09:16:27 -0500

    chnaged coord

--- navigation_coordinator/src/navigation_coordinator.cpp
+++ navigation_coordinator/src/navigation_coordinator.cpp
@@ -158,9 +158,9 @@ int main(int argc, char **argv)
     float y_3 = 0.416;
 
     float x_4 = 3.504;
-    float y_4 = 0.424;
+    float y_4 = 0.414;
 
-    float x_5 = -1.706;
+    float x_5 = -0.706;
     float y_5 = 1.75;
 
     float x_o = current_pose.pose.position.x;
